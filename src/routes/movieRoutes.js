e3cb15348d31605a1d1851ea4473c45e38ba32cc
Mayank-beast <mj683416@gmail.com>
2026-01-16 01:28:48 +0530

    initial commit

---
+++ src/routes/movieRoutes.js
@@ -0,0 +1,18 @@
+import express from 'express'; 
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({ message: 'get Movies endpoint' });
+});
+router.post('/', (req, res) => {
+    res.json({ message: 'post Movies endpoint' });
+});
+router.delete('/', (req, res) => {
+    res.json({ message: 'delete Movies endpoint' });
+});
+router.put('/', (req, res) => {
+    res.json({ message: 'put Movies endpoint' });
+});
+
+export default router; 
